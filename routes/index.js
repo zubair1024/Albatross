416fd5c84ec7707e51ae66bf50e2ab0d33f5957e
Zubair Ahmed <zubair.ahmed@roamworks.com>
2016-02-14 23:30:11 +0400

    * Adding forms

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Sim Inventory Management' });
+router.get('/', function (req, res, next) {
+    if (req.isAuthenticated()) {
+        res.render('dashboard', {title: ''});
+    } else {
+        res.render('login', {title: 'Sim Inventory Management'});
+    }
 });
 
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Register' });
+router.get('/register', function (req, res, next) {
+    res.render('register', {title: 'Register'});
+});
+
+router.get('/sim', function (req, res, next) {
+    if (req.isAuthenticated()) {
+        res.render('forms/sim', {title: 'SIM Panel'});
+    } else {
+        res.render('login');
+    }
 });
 
 
